56c851277bc7fcf4e3ba20332a89a1e92132a39d
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2025-02-23 00:56:33 +0530

    all origin allowed

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ wss.on('connection', (ws: WebSocket) => {
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL + "/*",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "authorization"],

24aee7ed16699d13a7e3a71ef7164013e8754ee2
Ravi Gangwar <113000419+ravi-gangwar@users.noreply.github.com>
2025-07-05 11:39:26 +0530

    fixing rate limiting

--- dist/index.js
+++ dist/index.js
@@ -12,12 +12,13 @@ const cors_1 = __importDefault(require("cors"));
 const ws_1 = require("ws");
 const handleMessage_1 = __importDefault(require("./ws/handleMessage"));
 const rateLiminting_1 = require("./middleware/rateLiminting");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const wss = new ws_1.WebSocketServer({ port: parseInt(process.env.WS_PORT || "5001") });
 const app = (0, express_1.default)();
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         (0, handleMessage_1.default)(message.toString(), ws);
     });
+    console.log("New connection");
 });
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
@@ -31,7 +32,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/v1/code", rateLiminting_1.rateLimiter, code_1.default);
 app.use("/api/v1/auth", rateLiminting_1.authRateLimiter, user_1.default);
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- src/middleware/rateLiminting.ts
+++ src/middleware/rateLiminting.ts
@@ -2,18 +2,18 @@ import rateLimit from 'express-rate-limit';
 
 // Create a rate limiter middleware
 export const rateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-    message: { message: 'Too many requests from this IP, please try again after 15 minutes' },
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 10, // Limit each IP to 10 requests per windowMs
+    message: { message: 'Too many requests from this IP, please try again after 5 minutes' },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 // Create a stricter rate limiter for authentication routes
 export const authRateLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 5, // Limit each IP to 5 requests per windowMs
-    message: { message: 'Too many login attempts, please try again after an hour' },
+    message: { message: 'Too many login attempts, please try again after 5 minutes' },
     standardHeaders: true,
     legacyHeaders: false,
     skipSuccessfulRequests: false, // Count all requests, including successful ones
